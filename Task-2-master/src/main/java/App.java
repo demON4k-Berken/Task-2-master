9866e824fc8ad10d22b9ed8d30c37abd54671659
demON4k-Berken <demON4k@mail.ru>
2025-05-10 11:43:54 +0300

    finish

--- Task-2-master/src/main/java/App.java
+++ Task-2-master/src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println();
+
+        Cat beanCatOne = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatOne.getName() + " " + beanCatOne.getAge());
+        Cat beanCatTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatTwo.getName() + " " + beanCatTwo.getAge());
+
+        System.out.println();
+
+        System.out.println(bean == bean2);
+        System.out.println(beanCatOne == beanCatTwo);
     }
 }
--- Task-2-master/src/main/java/AppConfig.java
+++ Task-2-master/src/main/java/AppConfig.java
@@ -1,14 +1,24 @@
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Cat cat() {
+        Cat cat = new Cat();
+        cat.setName("Тим");
+        cat.setAge(2);
+        return cat;
     }
 }
